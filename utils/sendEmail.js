ceb21467c78acc83ed31762ba6b6393e938d9280
Srivathsansundarrajan <srivathsansundarrajan@gmail.com>
2026-02-04 15:16:58 +0530

    changed port for the email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -9,7 +9,7 @@ if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
+  port: 465,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
